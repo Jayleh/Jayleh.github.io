f8a774cc367ea0eb96a1f3880f6c3d683b14f69c
Jayleh <jaylehyun@gmail.com>
2018-05-27 14:56:12 -0700

    added skillset json, bubble force simulation

--- js/resume.js
+++ js/resume.js
@@ -14,55 +14,99 @@ let items = new vis.DataSet([
     { id: 5, content: 'UCI Data Analytics & Visulization', start: '2018-01-18', style: educationStyle }
 ]);
 
-
-// Skillset data
-const LANGUAGE = [
-    ["Python", 10],
-    ["Javascript", 6],
-    ["mySQL", 4]
-];
-
-
-
-
-
 // Configuration for the timeline
 let options = {};
 
 // Create a Timeline
 let timeline = new vis.Timeline($container, items, options);
 
-// Set width and height of svg bubble chart
-let svgHeight = 500;
-let svgWidth = 500;
 
-// DOM element for skillset bubble chart
-let svg = d3.select('#skillset-plot')
-    .append('svg')
-    .attr('height', svgHeight)
-    .attr('width', svgWidth);
+// Force skillset diagram
+(function () {
+    // Set width and height of svg bubble chart
+    let svgHeight = 500,
+        svgWidth = 500;
 
-let radiusScale = d3.scaleSqrt()
-    .domain([0, 10])
-    .range([10, 50]);
+    // DOM element for skillset bubble chart
+    let svg = d3.select('#skillset-plot')
+        .append('svg')
+        .attr('height', svgHeight)
+        .attr('width', svgWidth);
 
-let chartGroup = svg.append('g')
-    .attr('transform', `translate(0, 0)`);
+    let chartGroup = svg.append('g')
+        .attr('transform', `translate(0, 0)`);
 
-chartGroup.selectAll('circle')
-    .data(LANGUAGE)
-    .enter()
-    .append('circle')
-    .attr('r', data => {
-        return data[1];
-    })
-    .attr("fill", `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`)
-    .attr("cx", data => {
-        return d.x
-    })
-    .attr("cy", data => {
-        return d.y
-    });
+    let radiusScale = d3.scaleSqrt()
+        .domain([4, 10])
+        .range([40, 60]);
+
+    // Simulation is a collection of forces about where we want our
+    // circles to go and how we want our circles to interact
+    // First, get them to the middle, then don't have them collide
+    let simulation = d3.forceSimulation()
+        .force('x', d3.forceX(svgWidth / 2).strength(0.05))
+        .force('y', d3.forceY(svgWidth / 2).strength(0.05))
+        .force('collide', d3.forceCollide(d => radiusScale(d.level + 0.1)));
+
+    // Grab skillset data json
+    d3.queue()
+        .defer(d3.json, "../js/data/skillset.json")
+        .await(ready); // calls the ready function
+
+    function ready(error, datapoints) {
+        if (error) throw error;
+
+        console.log(datapoints[0]);
+
+        let python = datapoints[0];
+        let frontend = datapoints[1];
+        let other = datapoints[2];
+
+        // Create circle group
+        let circles = chartGroup.append('g')
+            .selectAll('circle')
+            .data(python)
+            .enter()
+            .append('circle')
+            // .attr('r', data => data.level * 3)
+            .attr('r', d => radiusScale(d.level))
+            .attr("fill", `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`);
+
+        // Create text
+        let text = chartGroup.append('g')
+            .selectAll("text")
+            .data(python)
+            .enter()
+            .append("text")
+            .classed('text', true)
+            .text(d => d.name)
+            .style('font-size', 14)
+            .attr("text-anchor", "middle")
+            .attr("fill", "white");
+
+        simulation.nodes(python)
+            .on('tick', ticked);
+
+        function ticked() {
+            circles
+                .attr('cx', d => d.x)
+                .attr('cy', d => d.y);
+
+            text
+                .attr('x', d => d.x)
+                .attr('y', d => d.y);
+        }
+    }
+
+})(); // Initialize function on load
+
+
+
+
+
+
+
+
 
 
 
